f63a2efb04621e8fec09325da221e62f44058e42
NatachaLng <73877685+NatachaLng@users.noreply.github.com>
2020-12-07 20:06:45 +0100

    Validate GC & error message + corrections

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -29,3 +29,71 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+// Form elements
+var GeneralCondition = document.getElementById("GeneralCondition");
+var events = document.getElementsByTagName("location");
+
+var radioBtn = function (){
+  var events = document.getElementsByTagName("location"), i;
+  var checkee = false;
+  console.log(events.value);
+
+  }
+
+// Fonction Validate form
+function validate(){
+  console.log("validate");
+  formValid = true;
+  if (!GeneralCondition.checked){
+    document.getElementById('msgGC').style.display='block'; //display message
+    formValid=false; }
+    formValid=radioBtn(); 
+  console.log("sortie validate"+formValid);
+  return formValid;
+}
+
+/*var first=document.getElementById("first");
+first.addEventListener("keyup", function(event){
+if(first.validity.typeMismatch) {
+  first.setCustomValidity("Veuillez entrer 2 caractères ou plus pour le champ du prénom.");
+} else {
+  first.setCustomValidity("");
+}
+});
+
+var last=document.getElementById("last");
+last.addEventListener("keyup", function(event){
+if(last.validity.typeMismatch) {
+  last.setCustomValidity("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+} else {
+  last.setCustomValidity("");
+}
+});
+
+var email=document.getElementById("email");
+email.addEventListener("keyup", function(event){
+if(email.validity.typeMismatch) {
+  email.setCustomValidity("Vous devez entrer une adresse email valide");
+} else {
+  email.setCustomValidity("");
+}
+});
+
+var birthDate=document.getElementById("birthdate");
+birthDate.addEventListener("keyup", function(event){
+if(birthDate.validity.typeMismatch) {
+  birthDate.setCustomValidity("Vous devez entrer votre date de naissance");
+} else {
+  birthDate.setCustomValidity("");
+}
+});
+
+var numberOfEvents=document.getElementById("quantity");
+numberOfEvents.addEventListener("keyup", function(event){
+if(numberOfEvents.validity.typeMismatch) {
+  numberOfEvents.setCustomValidity("Vous devez entrer votre date de naissance");
+} else {
+  numberOfEvents.setCustomValidity("");
+}
+});*/
+
